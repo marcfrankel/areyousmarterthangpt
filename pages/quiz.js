92c8f761b1819eb77b4bee7b5ef63e71241a056c
marcfrankel <mfrankel218@gmail.com>
2023-04-01 15:59:28 -0400

    Initial home page

---
+++ pages/quiz.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Quiz() {
+    return (
+        <div>
+            <Head>
+                <title>Are You Smarter Than GPT?</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <h1>Quiz</h1>
+        </div>
+    );
+}
